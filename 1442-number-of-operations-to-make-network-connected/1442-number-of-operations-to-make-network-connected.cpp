6e3f29ff4929861637deafacb116e3321805dff4
Keshav1707 <127131373+Keshav1707@users.noreply.github.com>
2024-05-29 03:53:38 +0530

    Time: 83 ms (65.7%), Space: 46.1 MB (53.55%) - LeetHub

---
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,78 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    
+    }
+    
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int a=0;
+        for(auto i : connections)
+        {
+            if(ds.findUPar(i[0])==ds.findUPar(i[1]))
+            {
+                a++;
+            }
+            else ds.unionBySize(i[0],i[1]);
+        }
+        
+        int b=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ds.findUPar(i)==i)
+            {
+                b++;
+            }
+            
+        }
+        if(a<b-1)return -1;
+            else return b-1;
+    }
+};
