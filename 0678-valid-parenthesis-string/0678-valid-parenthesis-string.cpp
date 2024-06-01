3774762137d2f689ef498e1fab5e2f72ecd4ca3b
Keshav1707 <127131373+Keshav1707@users.noreply.github.com>
2024-06-02 04:48:54 +0530

    Time: 28 ms (5.11%), Space: 30.8 MB (5.01%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int dp[109][109];
+    bool rec(int ind,int a,string s)
+    {
+        if(ind==s.size())
+        {
+            if(a==0)return true;
+            else return false;
+        }
+        if(a<0)return false;
+        bool f=0;
+        if(dp[ind][a]!=-1)return dp[ind][a];
+        if(s[ind]=='(')f=f|rec(ind+1,a+1,s);
+        if(s[ind]==')')f=f|rec(ind+1,a-1,s);
+        if(s[ind]=='*')f=rec(ind+1,a+1,s)|rec(ind+1,a-1,s)|rec(ind+1,a,s);
+        return dp[ind][a]=f;
+    }
+    bool checkValidString(string s) {
+        memset(dp,-1,sizeof(dp));
+       return rec(0,0,s);
+    }
+};
