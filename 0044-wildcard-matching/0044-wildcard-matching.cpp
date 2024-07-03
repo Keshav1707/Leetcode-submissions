64e7000c885e677f5fd0690becff5156833fa7a8
Keshav Diwakar <keshavdewaker@gmail.com>
2024-07-04 02:14:10 +0530

    Time: 29 ms (73.95%), Space: 31.6 MB (19.97%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    bool rec(int inda, int indb, string &s, string &p, vector<vector<int>> &dp) {
-        if (dp[inda][indb] != -1) return dp[inda][indb];
-        
-        if (inda == s.size() && indb == p.size()) return dp[inda][indb] = true;
-        
-        if (indb == p.size() && inda < s.size()) return dp[inda][indb] = false;
-        
-        if (inda == s.size()) {
-            for (int i = indb; i < p.size(); i++) {
-                if (p[i] != '*') return dp[inda][indb] = false;
-            }
-            return dp[inda][indb] = true;
+vector<vector<int>>dp;
+bool rec(int inda,int indb,string&s,string&p)
+{
+    if(dp[inda][indb]!=-1)return dp[inda][indb];
+    if(inda==s.size()&&indb==p.size())return dp[inda][indb]=1;
+    if(indb==p.size()&&inda<s.size())return dp[inda][indb]=0;
+    if(inda==s.size())
+    {
+        for(int i=indb;i<p.size();i++)
+        {
+            if(p[i]!='*')return dp[inda][indb]=false;
         }
-        
-        if (s[inda] == p[indb] || p[indb] == '?') {
-            return dp[inda][indb] = rec(inda + 1, indb + 1, s, p, dp);
-        } 
-        
-        if (p[indb] == '*') {
-            return dp[inda][indb] = rec(inda + 1, indb, s, p, dp) || rec(inda, indb + 1, s, p, dp);
-        }
-        
-        return dp[inda][indb] = false;
+        return dp[inda][indb]=true;
     }
-    
+    if(s[inda]==p[indb]||p[indb]=='?')return dp[inda][indb]=rec(inda+1,indb+1,s,p);
+    else if(p[indb]=='*')return dp[inda][indb]= rec(inda+1,indb,s,p)||rec(inda,indb+1,s,p);
+    return dp[inda][indb]=0;
+}
+        
+       
     bool isMatch(string s, string p) {
-        int n = s.size(), m = p.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return rec(0, 0, s, p, dp);
+       dp=vector<vector<int>>(s.size()+1,vector<int>(p.size()+1,-1));
+     return rec(0,0,s,p);
     }
 };
