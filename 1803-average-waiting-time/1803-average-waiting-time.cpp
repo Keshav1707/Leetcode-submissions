fcca11e229ec7c6a855e5112c0e13f1862fc7e8c
Keshav Diwakar <keshavdewaker@gmail.com>
2024-07-09 05:44:52 +0530

    Time: 159 ms (35.88%), Space: 86.5 MB (12.94%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.cpp
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
       int curtime=customers[0][0];
       double sum=0;
       for(auto i : customers)
