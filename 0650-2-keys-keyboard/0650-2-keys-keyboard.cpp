5832bf5e4f8704afed8f243dde3b0c2cdbdca941
Keshav Diwakar <keshavdewaker@gmail.com>
2024-08-19 13:33:18 +0530

    Time: 17 ms (31.47%), Space: 11.9 MB (25.96%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
+int dp[1009][1009];
 int rec(int cur,int copied,int n)
 {
-    if(cur==n)
-    {
-        return 0;
-    }
+    if(cur==n)return 0;
     if(cur>n)return 1e9;
-    return min(1+rec(cur+copied,copied,n),2+rec(cur+cur,cur,n));
+    if(dp[cur][copied]!=-1)return dp[cur][copied];
+    return dp[cur][copied]=min(1+rec(cur+copied,copied,n),2+rec(cur+cur,cur,n));
 }
     int minSteps(int n) {
         if(n==1)return 0;
+        memset(dp,-1,sizeof(dp));
         return rec(1,1,n)+1;
     }
 };
