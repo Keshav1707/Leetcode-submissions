189886579f96d8a92deda96ff0b2a7ee5fca02ec
Keshav Diwakar <keshavdewaker@gmail.com>
2024-08-27 22:34:43 +0530

    Time: 4 ms (42.35%), Space: 9.4 MB (25.76%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-int dp[1009][59];
-int rec(int ind,vector<int>&v,int k)
-{
-    if(ind==v.size()&&k==0)return 0;
-    if(k==0)return 1e9;
-
-    if(dp[ind][k]!=-1)return dp[ind][k];
-    int sum=0;
-    int parts=INT_MIN;
-    int miniSum=INT_MAX;
-    for(int i=ind;i<v.size();i++)
-    {
-        sum+=v[i];
-        parts=max(sum,rec(i+1,v,k-1));
-        miniSum=min(miniSum,parts);
-    }
-    return dp[ind][k]=miniSum;
-}
     int splitArray(vector<int>& nums, int k) {
-        memset(dp,-1,sizeof(dp));
-        return rec(0,nums,k);
+        int n = nums.size();
+        int left = 0;
+        int right = 0;
+        for(int i = 0; i < n; i++)
+        {
+            left = max(left, nums[i]);
+            right += nums[i];
+        }
+        int mid = 0;
+        int ans = right;
+        while(left <= right)
+        {
+            mid = left + (right - left)/2;
+            int count = 1;
+            int temp = 0;
+            for(int i = 0; i < n; i++)
+            {
+                temp += nums[i];
+                if(temp > mid)
+                {
+                    count++;
+                    temp = nums[i];
+                }
+            }
+            if(count <= k)
+            {
+                ans = mid;
+                right = mid-1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return ans;
     }
 };
